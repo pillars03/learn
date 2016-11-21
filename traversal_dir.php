a3658d0f13e6e5d8c8c5a24e5767337603e8a7ff
pillars <625476086@qq.com>
2016-11-21 17:40:58 +0800

    遍历目录

---
+++ traversal_dir.php
@@ -0,0 +1,22 @@
+<?php
+function my_scandir($dir){
+    $files=array();
+    if(is_dir($dir)){
+        if($handle=opendir($dir)){
+            while(($file=readdir($handle))!==false){
+                if($file!="." && $file!=".."){
+                    if(is_dir($dir."/".$file)){
+                        $files[$file]=my_scandir($dir."/".$file);
+                    }
+                    else{
+                        $files[]=$dir."/".$file;
+                    }
+                }
+            }
+        }
+        closedir($handle);
+        return $files;
+    }
+}
+print_r(my_scandir('D:/data'));
+?>
